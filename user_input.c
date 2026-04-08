9485f6f0f12ec71186d79b60afbde075d0a645a7
Nillohit Roy <nillohitroy3@gmail.com>
2026-04-08 12:42:20 +0530

    Done Program for float inputs

--- user_input.c
+++ user_input.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+// For Integers
 	int a, b, sum;
 	printf("Enter the value of a: ");
 	scanf("%d", &a);
@@ -12,7 +13,15 @@ int main()
 	printf("\n");
 	
 	sum = a + b;
-	printf("The sum of two numbers %d and %d is: %d", a, b, sum);
+	printf("The sum of two numbers %d and %d is: %d\n", a, b, sum);
+	
+//	For Float
+	float num1, num2, sum1;
+	printf("Enter the num1 and num2: \n");
+	scanf("%f%f", &num1, &num2);
+	
+	sum1 = num1 + num2;
+	printf("The result is: %.3f", sum1);
 	
 	return 0;
 }
